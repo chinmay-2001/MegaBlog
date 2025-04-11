123680d776e67466963fe987b58e296ad7480c18
Chinmay <chinmaygumngaonkar2@gmail.com>
2025-04-11 10:19:34 +0530

    fixed file preview

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -107,7 +107,7 @@ export class Service {
   }
 
   getFilePreview(fileId) {
-    return this.bucket.getFilePreview(conf.appwriteBucketId, fileId);
+    return this.bucket.getFileView(conf.appwriteBucketId, fileId);
   }
 }
 
